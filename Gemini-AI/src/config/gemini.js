b37155ae676e420af418e50563e2d1c4d5201233
Prakhar Sharma <prakharsharma2002s@gmail.com>
2024-08-03 16:08:14 +0530

    done

--- Gemini-AI/src/config/gemini.js
+++ Gemini-AI/src/config/gemini.js
@@ -13,7 +13,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = "AIzaSyAcGC4Djmpqwm4He6b3J4_HQ9zC2x4hQzg"
+const apiKey = process.env.API_KEY
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
