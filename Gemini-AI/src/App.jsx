ff49b9e71a54a3e9797e9ac46b95865ad54e464e
Prakhar Sharma <prakharsharma2002s@gmail.com>
2024-08-03 16:03:24 +0530

    commit changes

---
+++ Gemini-AI/src/App.jsx
@@ -0,0 +1,14 @@
+import Main from "./components/Main/Main"
+import Sidebar from "./components/sidebar/Sidebar"
+
+
+const App = () => {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
